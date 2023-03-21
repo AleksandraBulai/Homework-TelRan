e3de58181f72415342d3de524de4cc5d9898c70d
AleksandraBulai <125496908+AleksandraBulai@users.noreply.github.com>
2023-03-21 19:33:20 +0100

    Add files via upload
    Создать класс Planet.
    В классе Planet создать переменную numberOfMoons типа int.
    В классе Planet создать переменную name типа String. 
    В классе Planet создать пустой конструктор по умолчанию.
    В классе Planet создать конструктор, принимающий параметр numberOfMoons , и записывающий его значение в переменную класса numberOfMoons.
    В классе Planet создать конструктор, принимающий параметр name , и записывающий его значение в переменную класса name., после чего вывести текст "Я - планета {подставить название планеты}".
    6. В классе Planet создать конструктор, принимающий параметры name и numberOfMoons , и записывающий их значения в переменные класса name и numberOfMoons.
    Создать класс с любым названием и методом "main".
    В методе "main" создать 5 разных объектов типа Planet.
    При создании использовать различные написанные вами конструкторы.
    Вывести поля numberOfMoons и name у каждой из созданных планет.

---
+++ Java/HW2103/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Planet saturn = new Planet();
+        Planet earth = new Planet("Земля", 1);
+        Planet mars = new Planet(2);
+        Planet neptune = new Planet("Нептун", 14);
+        Planet uranus = new Planet("Уран");
+
+
+    }
+}
+
